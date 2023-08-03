301f5d7ce9fe661de04df6698cac4a4623d3e736
Michael Cukingnan <michael.cukingnan@gmail.com>
2023-08-02 23:00:37 -0700

    storyblok-integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
 
-// https://astro.build/config
-export default defineConfig({})
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
+export default defineConfig({
+  redirects: {
+    '/': '/home',
+  },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        details: 'storyblok/Details',
+        page: 'storyblok/Page',
+        title: 'storyblok/Title',
+      },
+      apiOptions: {
+        region: 'us',
+      },
+    }),
+  ],
+})
