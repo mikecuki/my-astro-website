3aa66ffa1e2feac1ea3dd9b7f8e60da2f17b2df7
Michael Cukingnan <michael.cukingnan@gmail.com>
2023-08-03 19:14:01 -0700

    Build fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,30 @@
 import { defineConfig } from 'astro/config'
 import storyblok from '@storyblok/astro'
 import { loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 export default defineConfig({
-  redirects: {
-    '/': '/home',
-  },
   integrations: [
     storyblok({
+      // Access token must be updated between spaces
       accessToken: env.STORYBLOK_TOKEN,
       components: {
-        details: 'storyblok/Details',
         page: 'storyblok/Page',
-        title: 'storyblok/Title',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
       },
       apiOptions: {
         region: 'us',
       },
     }),
   ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 })
