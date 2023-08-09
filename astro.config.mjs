30592196152b6a0fdd1df3d00f300f5f10a86217
Michael Cukingnan <michael.cukingnan@gmail.com>
2023-08-08 18:32:10 -0700

    Fix netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify/functions'
 import storyblok from '@storyblok/astro'
 import { loadEnv } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
@@ -6,18 +7,21 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     storyblok({
       // Access token must be updated when changing spaces in storyblok
       accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
+      apiOptions: {
+        region: 'us',
+      },
       components: {
         page: 'storyblok/Page',
         feature: 'storyblok/Feature',
         grid: 'storyblok/Grid',
         teaser: 'storyblok/Teaser',
-      },
-      apiOptions: {
-        region: 'us',
       },
     }),
   ],
